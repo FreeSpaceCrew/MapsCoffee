cbf1757657fb2ec054ad16b6c94823d0e8639ccc
Agrass <Agrass_3840@gmail.com>
2016-02-10 23:56:46 +0300

    added elastic search

--- app/controllers/API.java
+++ app/controllers/API.java
@@ -1,14 +1,62 @@
 package controllers;
 
+import classes.BoundingBox;
+import play.libs.F;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import javax.inject.Inject;
+
 public class API extends Controller {
 
+    @Inject
+    WSClient ws;
 
-    public Result points(String s, String n, String w, String e) {
-        return ok(index.render(String.format("s = %s, n = %s, w = %s, e = %s", s, n, w, e)));
+    private static final String ELASTIC_URL = "http://localhost:9200/map/coffee/_search";
+
+    public F.Promise<Result> points(String s, String n, String w, String e) {
+
+        WSRequest request = ws.url(ELASTIC_URL);
+        F.Promise<WSResponse> responsePromise = request.post(getRequestBody(s, w, n, e));
+
+
+        BoundingBox boundingBox = new BoundingBox(s, w, n, e);
+        F.Promise<BoundingBox> promise = F.Promise.promise(() -> boundingBox);
+
+        return responsePromise.map(response -> ok(response.asJson()));
+//        return promise.map(box -> ok(index.render("S value is " + s + " N value is " + n + " W:"+ w + "E: "+ e)));
+    }
+
+    private String getRequestBody(String s, String w, String n, String e) {
+        return String.format("{\n" +
+                        " \"size\" : 1000," +   // number of documents (points)\n
+                        " \"query\":{\n" +
+                        "    \"bool\" : {\n" +
+                        "        \"must\" : {\n" +
+                        "            \"match_all\" : {}" +             // search query (empty match all documents)\n
+                        "         },\n" +
+                        "        \"filter\" : {\n" +
+                        "            \"geo_bounding_box\" : {" +       // bbox (s,w) -> (n,e)\n"
+                        "                \"location\" : {\n" +
+                        "                    \"top_left\" : {\n" +
+                        "                        \"lat\" : %s,\n" + //s
+                        "                        \"lon\" : %s\n" +  //w
+                        "                    },\n" +
+                        "                    \"bottom_right\" : {\n" +
+                        "                        \"lat\" : %s,\n" + //n
+                        "                        \"lon\" : %s\n" + //e
+                        "                    }\n" +
+                        "                }\n" +
+                        "            }\n" +
+                        "        }\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}",
+                s, w, n, e);
     }
 
 
---
+++ scripts/fill_index.pl
@@ -0,0 +1,74 @@
+use strict;
+use warnings;
+use utf8;
+use feature 'say';
+
+use Mojo::UserAgent;
+use Data::Dumper;
+use XML::Simple;
+
+
+my $ua = Mojo::UserAgent->new;
+my $xs = XML::Simple->new;
+
+my $n = $ARGV[0] || 56.103704;
+my $s = $ARGV[1] || 55.225368;
+my $e = $ARGV[2] || 38.106079;
+my $w = $ARGV[3] || 37.117309;
+
+
+# 54.831281&s=56.585708&e=38.609619&w=36.63208
+my $body = "<bbox-query s=\"$s\" n=\"$n\" w=\"$w\" e=\"$e\"/>
+         <query type=\"node\">
+            <item/>
+            <has-kv k=\"amenity\" v=\"cafe\"/>
+            <has-kv k=\"cuisine\" v=\"coffee_shop\"/>
+         </query>
+         <print/>";
+
+say $body;
+
+my $xml = $ua->post("http://overpass-api.de/api/interpreter" => {Accept => '*/*'} => $body)->res->body;
+
+say "found: ".scalar(keys %{$xs->XMLin($xml)->{node}});
+
+foreach my $id (%{$xs->XMLin($xml)->{node}}) {
+    #say $id;
+
+    my $tags    = $xs->XMLin($xml)->{node}{$id}{tag};
+    my $name    = get_tag('name:ru', @$tags) || get_tag('name', @$tags);
+    my $lat     = $xs->XMLin($xml)->{node}{$id}{lat};
+    my $lon     = $xs->XMLin($xml)->{node}{$id}{lon};
+    my $hours   = get_tag('opening_hours', @$tags);
+
+    if($name && $lat && $lon && $hours) {
+        #say $name;
+        #say $lat;
+        #say $lon;
+
+        my $json = "{
+            \"name\": \"$name\",
+            \"opening_hours\": \"$hours\",
+            \"location\" : {
+                \"lat\": $lat,
+                \"lon\": $lon
+            }
+        }";
+
+# say Dumper $json;
+
+        my $tx = $ua->put("http://localhost:9200/map/coffee/$id" => {Accept => '*/*'} => $json);
+        #say Dumper $tx->res->body;
+        # last;
+    }
+
+
+}
+
+sub get_tag {
+    my ($name, @a) = @_;
+
+    my @tag = grep { $_->{k} eq $name } @a;
+
+    return $tag[0]->{v};
+}
